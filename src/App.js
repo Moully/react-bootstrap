c04c4b2a06314ba22763329121bbc25637e597a4
mstars_lab2_12 <lab_02_02@Lab-02-02s-iMac.local>
2022-03-28 17:44:51 +0800

    carousel immage added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap"
 
 function App() {
   return (
+    <>
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
@@ -24,6 +25,47 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1214757157.png?crop=1.00xw:0.751xh;0.00160xw,0.134xh&resize=980:*"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-kimchee-royalty-free-image-1621288320.?crop=0.668xw:1.00xh;0.138xw,0&resize=768:*"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/soba-royalty-free-image-1621288461.?crop=0.669xw:1.00xh;0.164xw,0&resize=768:*"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    </>
+
+    
   );
 }
 
